8b30837a582a40193fe96bedd5af7930a10d1829
vivektarun <vivektarun1234@gmail.com>
2024-04-01 22:50:50 +0530

    feat: basic error handling done

--- src/controllers/problem.controller.js
+++ src/controllers/problem.controller.js
@@ -1,27 +1,47 @@
-const { StatusCodes } = require('http-status-codes');
+const NotImplemented = require('../errors/notImplemented.error');
 
 function pingProblemController(req, res) {
     return res.json({message: 'problem controller is up'});
 }
 
 function addProblem(req, res) {
-    return res.status(StatusCodes.NOT_IMPLEMENTED).json({message: "Not Implemented"});
+    try {
+        throw new NotImplemented('addproblem');
+    } catch(error) {
+        next(error);
+    }
 }
 
 function getProblem(req, res) {
-    return res.status(StatusCodes.NOT_IMPLEMENTED).json({message: "Not Implemented"});
+    try {
+        throw new NotImplemented();
+    } catch(error) {
+        next(error);
+    }
 }
 
 function getProblems(req, res) {
-    return res.status(StatusCodes.NOT_IMPLEMENTED).json({message: "Not Implemented"});
+    try {
+        throw new NotImplemented();
+    } catch(error) {
+        next(error);
+    }
 }
 
 function deleteProblem(req, res) {
-    return res.status(StatusCodes.NOT_IMPLEMENTED).json({message: "Not Implemented"});
+    try {
+        throw new NotImplemented();
+    } catch(error) {
+        next(error);
+    }
 }
 
 function updateProblem(req, res) {
-    return res.status(StatusCodes.NOT_IMPLEMENTED).json({message: "Not Implemented"});
+    try {
+        throw new NotImplemented();
+    } catch(error) {
+        next(error);
+    }
 }
 
 
